ab8eccb664e1299d69a19f7d22817f22c8268b9f
Dezolder <evgeniy.zhukov.2012@gmail.com>
2023-03-12 12:06:30 +0200

    fix

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -1,16 +1,18 @@
 import { useState } from "react"
 
-export function useLocalStorage(key, token = '') {
-  const [value, setValue] = useState(token)
+export function useLocalStorage(key) {
+  const [token, setToken] = useState(
+    localStorage.getItem(key) || ''
+  )
 
   function setItem(data) {
     localStorage.setItem(key, data)
-    setValue(data)
+    setToken(data)
   }
 
   function removeItem() {
     localStorage.removeItem(key)
-    setValue('')
+    setToken('')
   }
-  return [value, { setItem, removeItem }]
+  return [token, { setItem, removeItem }]
 }
